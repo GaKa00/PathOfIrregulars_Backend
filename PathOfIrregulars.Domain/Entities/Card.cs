236744338a0cf8c203d1d7e69ef52e195c18892d
Gabriel Kassarp <gkassarp@gmail.com>
2026-04-27 17:18:00 +0200

    added description to clone

--- PathOfIrregulars.Domain/Entities/Card.cs
+++ PathOfIrregulars.Domain/Entities/Card.cs
@@ -51,6 +51,7 @@ namespace PathOfIrregulars.Domain.Entities
             {
                 Id = this.Id,
                 Name = this.Name,
+                Description = this.Description,
                 Type = this.Type,
                 Power= this.Power,
                     
